8e23ff8027e03f1e2d5f5b2baf70d4eae63b7bd4
artur323 <yummy.radish.x@gmail.com>
2024-03-31 14:22:19 -0700

    feat: landing page with dark mode

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <div class="bg-snowwhite"> -->
+  <div :class="{'dark:bg-darkBackground': darkMode}">
+    <Index />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import Index from "./Pages/Palletspace/Landingpage/Index.vue";
+import { darkMode } from './theme.js';
+
+onMounted(() => {
+  if (darkMode.value) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+});
+
+</script>
---
+++ src/Pages/Palletspace/Landingpage/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <NavigationSection />
+    <PalletspaceHeroSection />
+    <ProcessSection />
+    <CustomerLogoSection />
+    <TrackRecordSection />
+    <PartnerLogoSection />
+    <HowToSection />
+    <FaqSection />
+    <SupportSection />
+    <LeaderShipSection />
+    <FooterSection />
+</template>
+
+<script setup>
+import NavigationSection from "../../../Components/Organisms/NavigationSection.vue";
+import PalletspaceHeroSection from "../../../Components/Organisms/PalletspaceHeroSection.vue";
+import ProcessSection from "../../../Components/Organisms/ProcessSection.vue";
+import CustomerLogoSection from "../../../Components/Organisms/CustomerLogoSection.vue";
+import PartnerLogoSection from "../../../Components/Organisms/PartnerLogoSection.vue";
+import FaqSection from "../../../Components/Organisms/FaqSection.vue";
+import FooterSection from "../../../Components/Organisms/FooterSection.vue";
+import LeaderShipSection from "../../../Components/Organisms/LeaderShipSection.vue";
+import SupportSection from "../../../Components/Organisms/SupportSection.vue";
+import TrackRecordSection from "../../../Components/Organisms/TrackRecordSection.vue";
+import HowToSection from "../../../Components/Organisms/HowToSection.vue";
+</script>
