93c71b283c9ad186fd376884184d60e8c0931883
Delicious Radish <yummy.radish.x@gmail.com>
2024-04-01 19:38:11 +0900

    feat: break down with Atom structure on Track Record Section

---
+++ src/Components/Atoms/StatItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col gap-y-3 border-l border-white/10 pl-6">
+    <dt class="text-sm leading-6">{{ name }}</dt>
+    <dd class="order-first text-3xl font-semibold tracking-tight">{{ value }}</dd>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  name: String,
+  value: String,
+});
+</script>
