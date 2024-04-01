b357db19956d21bd5192133041465b043b7a8e1d
Delicious Radish <yummy.radish.x@gmail.com>
2024-04-01 23:48:31 +0900

    feat: footer with atom structure

---
+++ src/Components/Molecules/FooterNavItemList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="class">
+    <h3 class="text-sm font-semibold leading-6 text-gray-900 dark:text-snowwhite">{{ title }}</h3>
+    <ul role="list" class="mt-6 space-y-4">
+      <li v-for="item in items" :key="item.name">
+        <a :href="item.href"
+          class="text-sm leading-6 text-gray-600 hover:text-gray-900 dark:text-gray-500 dark:hover:text-gray-400">{{ item.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: String,
+  items: Array,
+  class: {
+    type: String,
+    default: ""
+  }
+});
+</script>
---
+++ src/Components/Molecules/SocialLinkList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex space-x-6">
+    <a v-for="item in socialLinks" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
+      <span class="sr-only">{{ item.name }}</span>
+      <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  socialLinks: Array
+});
+</script>
