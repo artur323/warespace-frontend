f54862184a7afa75ad72d1379e92326546c2d7df
artur323 <yummy.radish.x@gmail.com>
2024-03-31 23:47:51 -0700

    fix: header font family

--- src/Components/Organisms/HowToSection.vue
+++ src/Components/Organisms/HowToSection.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="font-poppins" :class="{ 'dark:bg-darkBackground' : darkMode }">
     <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-      <div class="relative isolate overflow-hidden bg-gray-900 dark:bg-snowwhite px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
-        <h2 class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl dark:text-gray-900">Boost your productivity today.</h2>
+      <div class="relative isolate overflow-hidden bg-gray-900 dark:bg-[#1e293b] px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+        <h2 class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl dark:text-snowwhite">Boost your productivity today.</h2>
         <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300 dark:text-gray-500">Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 dark:bg-gradient-to-r dark:from-[#0C338D] dark:to-[#4F5BD3] dark:text-snowwhite shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
-          <a href="#" class="text-sm font-semibold leading-6 text-white dark:text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+          <a href="#" class="text-sm font-semibold leading-6 text-white dark:text-snowwhite">Learn more <span aria-hidden="true">→</span></a>
         </div>
         <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
           <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7" />
