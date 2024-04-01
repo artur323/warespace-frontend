ba00a4ee737267225984db087f881e0c1628a1cf
Delicious Radish <yummy.radish.x@gmail.com>
2024-04-01 20:26:41 +0900

    feat: break down with Atom structure on Partner Logo Section

--- src/Components/Atoms/LogoItem.vue
+++ src/Components/Atoms/LogoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="imageSrc" :alt="alt" class="max-h-12 w-full object-contain object-left" />
+  <img :src="imageSrc" :alt="alt" :class="class" :width="width" :height="height"/>
 </template>
 
 <script setup>
@@ -8,5 +8,17 @@ import { defineProps } from 'vue';
 const props = defineProps({
   imageSrc: String,
   alt: String,
+  class: {
+    type: String,
+    default: "max-h-12 w-full object-contain object-left"
+  },
+  width: {
+    type: String,
+    default: "auto"
+  },
+  height: {
+    type: String,
+    default: "auto"
+  }
 });
 </script>
--- src/data.js
+++ src/data.js
@@ -2,7 +2,23 @@ import LightLogo from "@/assets/img/light-logo.png";
 import DarkLogo from "@/assets/img/dark-logo.webp";
 import LightModeSvg from "@/assets/icons/light-mode.svg";
 import DarkModeSvg from "@/assets/icons/dark-mode.svg";
+import { darkMode } from "@/theme";
+
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid';
+
+const lightTupleSVG = "https://tailwindui.com/img/logos/tuple-logo-gray-900.svg";
+const lightReformSVG = "https://tailwindui.com/img/logos/reform-logo-gray-900.svg";
+const lightSavvycalSVG = "https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg";
+const lightLaravelSVG = "https://tailwindui.com/img/logos/laravel-logo-gray-900.svg";
+const lightTransistorSVG = "https://tailwindui.com/img/logos/transistor-logo-gray-900.svg";
+const lightStatamicSVG = "https://tailwindui.com/img/logos/statamic-logo-gray-900.svg";
+
+const darkTupleSVG = "https://tailwindui.com/img/logos/tuple-logo-white.svg";
+const darkReformSVG = "https://tailwindui.com/img/logos/reform-logo-white.svg";
+const darkSavvycalSVG = "https://tailwindui.com/img/logos/savvycal-logo-white.svg";
+const darkLaravelSVG = "https://tailwindui.com/img/logos/laravel-logo-white.svg";
+const darkTransistorSVG = "https://tailwindui.com/img/logos/transistor-logo-white.svg";
+const darkStatamicSVG = "https://tailwindui.com/img/logos/statamic-logo-white.svg";
 
 export const navigation = [
   { name: 'Product', href: '#' },
@@ -38,6 +54,19 @@ export const logos = [
   { light: "https://tailwindui.com/img/logos/statamic-logo-gray-900.svg", dark: "https://tailwindui.com/img/logos/statamic-logo-white.svg", alt: "Statamic" },
 ];
 
+export const lightLogo = LightLogo;
+export const darkLogo = DarkLogo;
+export const lightModeSvg = LightModeSvg;
+export const darkModeSvg = DarkModeSvg;
+
+export const logoRows = [
+  { dark: darkTransistorSVG, light : lightTransistorSVG, alt: 'Transistor', width: '158', height: '48', class: 'col-span-2 max-h-12 w-full object-contain lg:col-span-1' },
+  { dark: darkReformSVG, light : lightReformSVG, alt: 'Reform', width: '158', height: '48', class: 'col-span-2 max-h-12 w-full object-contain lg:col-span-1' },
+  { dark: darkTupleSVG, light : lightTupleSVG, alt: 'Tuple', width: '158', height: '48', class: 'col-span-2 max-h-12 w-full object-contain lg:col-span-1' },
+  { dark: darkSavvycalSVG, light : lightSavvycalSVG, alt: 'SavvyCal', width: '158', height: '48', class: 'col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1' },
+  { dark: darkStatamicSVG, light : lightStatamicSVG, alt: 'Statamic', width: '158', height: '48', class: 'col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1' }
+];
+
 export const stats = [
   { id: 1, name: 'Creators on the platform', value: '8,000+' },
   { id: 2, name: 'Flat platform fee', value: '3%' },
@@ -45,7 +74,4 @@ export const stats = [
   { id: 4, name: 'Paid out to creators', value: '$70M' },
 ];
 
-export const lightLogo = LightLogo;
-export const darkLogo = DarkLogo;
-export const lightModeSvg = LightModeSvg;
-export const darkModeSvg = DarkModeSvg;
+
